575b1938eb67ff5cf70d2656726de882e651605c
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2023-02-06 16:58:46 -0300

    criação do enpoint /talkers/:id

--- src/index.js
+++ src/index.js
@@ -1,29 +1,39 @@
 const express = require('express');
-const fs = require('fs/promises');
+// const fs = require('fs/promises');
+
+const talkersListRouter = require('./routes/talkersListRoutes');
 
 const app = express();
 app.use(express.json());
 
+app.use(talkersListRouter);
+
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
-const readMyJSON = async () => {
-  const myJSON = await fs.readFile('src/talker.json', 'utf8');
-  return JSON.parse(myJSON);
-};
+// const readMyJSON = async () => {
+//   const myJSON = await fs.readFile('src/talker.json', 'utf8');
+//   return JSON.parse(myJSON);
+// };
 
+// app.get('/talker', async (_req, res) => {
+//   const talkersList = await readMyJSON();
 
-app.get('/talker', async (_req, res) => {
-  const talkersList = await readMyJSON();
+//   if (talkersList.lenght === 0) {
+//     return res.status(HTTP_OK_STATUS).json([]);
+//   } 
+//   res.status(HTTP_OK_STATUS).send(talkersList);
+// });
 
-  if (talkersList.lenght === 0) {
-    return res.status(HTTP_OK_STATUS).json([]);
-  } else {
-    res.status(HTTP_OK_STATUS).send(talkersList);
-  }
-  
-});
-
+// app.get('/talker/:id', async (req, res) => {
+//   const targetID = Number(req.params.id);
+//   const talkersList = await readMyJSON();
+//   const getByID = talkersList.find((talker) => talker.id === targetID);
+//   if (!getByID){
+//     return  res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+//   }
+//   res.status(HTTP_OK_STATUS).send(getByID);
+// });
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
---
+++ src/routes/talkersListRoutes.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const fs = require('fs/promises');
+
+const talkersListRouter = express.Router();
+
+const HTTP_OK_STATUS = 200;
+
+const readMyJSON = async () => {
+  const myJSON = await fs.readFile('src/talker.json', 'utf8');
+  return JSON.parse(myJSON);
+};
+
+talkersListRouter.get('/talker', async (_req, res) => {
+  const talkersList = await readMyJSON();
+
+  if (talkersList.lenght === 0) {
+    return res.status(HTTP_OK_STATUS).json([]);
+  } 
+  res.status(HTTP_OK_STATUS).send(talkersList);
+});
+
+talkersListRouter.get('/talker/:id', async (req, res) => {
+  const targetID = Number(req.params.id);
+  const talkersList = await readMyJSON();
+  const getByID = talkersList.find((talker) => talker.id === targetID);
+  if (!getByID){
+    return  res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  res.status(HTTP_OK_STATUS).send(getByID);
+});
+
+module.exports = talkersListRouter;
