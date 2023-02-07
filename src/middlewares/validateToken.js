8b975b3fbd42eecb62e81e4b806d85224492743f
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2023-02-07 09:13:18 -0300

    criação do middleware de validação do token

---
+++ src/middlewares/validateToken.js
@@ -0,0 +1,15 @@
+const HTTP_UNAUTHORIZED_STATUS = 401;
+
+const validateToken = (req,res,next) => {
+  const { authorization } = req.headers;
+  const authValue = Object.values(authorization);
+
+  if (!authorization) {
+    return res.status(HTTP_UNAUTHORIZED_STATUS).json({ message: 'Token não encontrado'})
+  } 
+  if (authValue.length !== 16 && typeof authValue !== 'string') {
+    return res.status(HTTP_UNAUTHORIZED_STATUS).JASON({ message: 'Token inválido' })
+  }
+};
+
+module.exports = validateToken;
