f4ee40a1365a0c2f900fab66c83b0333f5c81794
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2023-02-07 09:50:23 -0300

    correção de erros do lint

--- src/middlewares/validateTalkerAge.js
+++ src/middlewares/validateTalkerAge.js
@@ -1,20 +1,20 @@
-const HTTP_BADREQUEST_STATUS = 400;
+const BADREQ = 400;
 
 const validateTalkerAge = (req, res, next) => {
   const { age } = req.body;
   const ageValue = Object.values(age);
 
   if (!age) {
-    return res.status(HTTP_BADREQUEST_STATUS).json({ message: 'O campo "age" é obrigatório' });
+    return res.status(BADREQ).json({ message: 'O campo "age" é obrigatório' });
   }
-  if ( typeof ageValue !== 'number') {
-    return res.status(HTTP_BADREQUEST_STATUS).json({ message: 'O campo "age" deve ser do tipo "number"' })
+  if (typeof ageValue !== 'number') {
+    return res.status(BADREQ).json({ message: 'O campo "age" deve ser do tipo "number"' });
   }
   if (!Number.isInteger(ageValue)) {
-    return res.status(HTTP_BADREQUEST_STATUS).json({ message: 'O campo "age" deve ser "number" do tipo inteiro' })
+    return res.status(BADREQ).json({ message: 'O campo "age" deve ser "number" do tipo inteiro' });
   }
-  if ( ageValue < 18) {
-    return res.status(HTTP_BADREQUEST_STATUS).json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  if (ageValue < 18) {
+    return res.status(BADREQ).json({ message: 'A pessoa palestrante deve ser maior de idade' });
   }
   return next();
 };
--- src/middlewares/validateTalkerName.js
+++ src/middlewares/validateTalkerName.js
@@ -1,14 +1,14 @@
-const HTTP_BADREQUEST_STATUS = 400;
+const BADREQUEST = 400;
 
 const validadeTalkerName = (req, res, next) => {
   const { name } = req.body;
   const nameValue = Object.values(name);
 
   if (!name) {
-    return res.status(HTTP_BADREQUEST_STATUS).json({ message: 'O campo "name" é obrigatório' })
+    return res.status(BADREQUEST).json({ message: 'O campo "name" é obrigatório' });
   }
   if (nameValue.length < 3) {
-    return res.status(HTTP_BADREQUEST_STATUS).json({ message: 'O "name" deve ter pelo menos 3 caracteres' })
+    return res.status(BADREQUEST).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
   }
   return next();
 };
