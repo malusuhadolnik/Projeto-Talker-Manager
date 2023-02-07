702943eb4c36a1cd8fed4ad84fcf820fa38b7f9a
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2023-02-07 09:41:53 -0300

    criação do middleware de validação da idade do palestrante

---
+++ src/middlewares/validateTalkerAge.js
@@ -0,0 +1,22 @@
+const HTTP_BADREQUEST_STATUS = 400;
+
+const validateTalkerAge = (req, res, next) => {
+  const { age } = req.body;
+  const ageValue = Object.values(age);
+
+  if (!age) {
+    return res.status(HTTP_BADREQUEST_STATUS).json({ message: 'O campo "age" é obrigatório' });
+  }
+  if ( typeof ageValue !== 'number') {
+    return res.status(HTTP_BADREQUEST_STATUS).json({ message: 'O campo "age" deve ser do tipo "number"' })
+  }
+  if (!Number.isInteger(ageValue)) {
+    return res.status(HTTP_BADREQUEST_STATUS).json({ message: 'O campo "age" deve ser "number" do tipo inteiro' })
+  }
+  if ( ageValue < 18) {
+    return res.status(HTTP_BADREQUEST_STATUS).json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+  return next();
+};
+
+module.exports = validateTalkerAge;
--- src/middlewares/validateTalkerName.js
+++ src/middlewares/validateTalkerName.js
@@ -1,9 +1,8 @@
 const HTTP_BADREQUEST_STATUS = 400;
 
 const validadeTalkerName = (req, res, next) => {
-  const { name, age } = req.body;
+  const { name } = req.body;
   const nameValue = Object.values(name);
-  const ageValue = Object.values(age);
 
   if (!name) {
     return res.status(HTTP_BADREQUEST_STATUS).json({ message: 'O campo "name" é obrigatório' })
